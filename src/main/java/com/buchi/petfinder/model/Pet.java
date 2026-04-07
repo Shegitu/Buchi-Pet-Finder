b6355ac67279d0219f9ed66343e5c30786f41d89
shegitu <physicistshegitu@gmail.com>
2026-04-07 08:16:01 -0700

    Created Pet model for MongoDB collection

--- src/main/java/com/buchi/petfinder/model/Pet.java
+++ src/main/java/com/buchi/petfinder/model/Pet.java
@@ -1,19 +1,21 @@
 package com.buchi.petfinder.model;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Document(collection = "pets")
+
 public class Pet {
     @Id
     private String id;
     private String name;
     private String type;
     private int age;
+    private String owner;
 }
