8c3f53a7bfdc1e9a964ed9cea43c31396885c0f4
shegitu <physicistshegitu@gmail.com>
2026-04-07 08:14:36 -0700

    Added main Spring Boot application class

--- src/main/java/com/buchi/petfinder/PetfinderApplication.java
+++ src/main/java/com/buchi/petfinder/PetfinderApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PetfinderApplication {
 
+public class PetfinderApplication {
     public static void main(String[] args) {
         SpringApplication.run(PetfinderApplication.class, args);
     }
