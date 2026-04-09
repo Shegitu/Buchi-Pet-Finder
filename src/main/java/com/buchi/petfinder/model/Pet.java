96cd998e39424a5450969a0c1cfefaeff13e68ee
shegitu <physicistshegitu@gmail.com>
2026-04-09 02:09:54 -0700

    Add validation annotations to Pet model

--- src/main/java/com/buchi/petfinder/model/Pet.java
+++ src/main/java/com/buchi/petfinder/model/Pet.java
@@ -1,5 +1,7 @@
 package com.buchi.petfinder.model;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import lombok.AllArgsConstructor;
@@ -10,12 +12,19 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Document(collection = "pets")
-
 public class Pet {
+
     @Id
     private String id;
+
+    @NotBlank(message = "Name is required")
     private String name;
+
+    @NotBlank(message = "Type is required")
     private String type;
+
+    @Min(value = 0, message = "Age must be non-negative")
     private int age;
+
     private String owner;
 }
